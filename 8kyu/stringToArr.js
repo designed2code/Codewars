8036728511957b81df3692c775b559eeda248696
designed2code <88329785+designed2code@users.noreply.github.com>
2021-08-11 19:13:59 +0530

    Create stringToArr.js

---
+++ 8kyu/stringToArr.js
@@ -0,0 +1,9 @@
+// https://www.codewars.com/kata/57e76bc428d6fbc2d500036d/train/javascript
+// # NOTE: defining the SEPARATOR, where to split the string is necessary
+function stringToArray(string){
+  return string.split(' ') //Split the string at spaces
+}
+
+stringToArray("John Doe")
+//OUTPUT
+// ["John", "Doe"]
