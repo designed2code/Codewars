82c7be1bb2e66f74f55e7f74555c7971cb328662
designed2code <88329785+designed2code@users.noreply.github.com>
2021-08-13 23:33:12 +0530

    Added a comment

--- 6kyu/digitalRoot.js
+++ 6kyu/digitalRoot.js
@@ -1,5 +1,5 @@
 // https://www.codewars.com/kata/541c8630095125aba6000c00/train/javascript
-// Referred This Article 
+// Referred This Article https://people.revoledu.com/kardi/tutorial/DigitSum/digital%20root.html
 function digital_root(n){
   return (n-1) % 9 + 1 ;
 }
