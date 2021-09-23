3443be1073773462ebb0887a4d83f6a70f818192
Sachin Yadav <88329785+designed2code@users.noreply.github.com>
2021-09-23 13:45:33 +0530

    Appending short & long str based on length

---
+++ 8kyu/shortLongShort.js
@@ -0,0 +1,7 @@
+// https://www.codewars.com/kata/50654ddff44f800200000007/train/javascript
+// Short Long Short
+
+function solution(a, b){
+  // your code here
+  return a.length < b.length ? (a+b+a) : (b+a+b) 
+}
