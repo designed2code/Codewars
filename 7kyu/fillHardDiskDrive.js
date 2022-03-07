91899bfb661c27ca88a1488188a68183c1ea1db7
Sachin Yadav <88329785+designed2code@users.noreply.github.com>
2022-03-07 20:59:23 +0530

    Create fillHardDiskDrive.js

---
+++ 7kyu/fillHardDiskDrive.js
@@ -0,0 +1,27 @@
+Your task is to determine how many files of the copy queue you will be able to save into your Hard Disk Drive. The files must be saved in the order they appear in the queue.
+Question Link https://www.codewars.com/kata/5d49c93d089c6e000ff8428c
+Input:
+Array of file sizes (0 <= s <= 100)
+Capacity of the HD (0 <= c <= 500)
+Output:
+Number of files that can be fully saved in the HD.
+Examples:
+save([4,4,4,3,3], 12) -> 3
+# 4+4+4 <= 12, but 4+4+4+3 > 12
+save([4,4,4,3,3], 11) -> 2
+# 4+4 <= 11, but 4+4+4 > 11
+Do not expect any negative or invalid inputs.
+
+// Code
+function save(sizes, hd) {
+  let sumOfFiles = 0
+  let numberOfFiles = 0
+  for(let i =0;i<=sizes.length-1;i++){
+    sumOfFiles = sizes[i] + sumOfFiles
+    if(sumOfFiles > hd){
+      return numberOfFiles++
+    }
+    numberOfFiles++
+  }
+  return numberOfFiles
+}
